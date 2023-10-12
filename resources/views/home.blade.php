5c91ca9b9342c955ac9bf61f73b0bc5e7dc1bddb
Pis4ni <pis4wrk@gmail.com>
2023-10-12 14:35:51 +0200

    add: working data trasnsfer from 'db'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,26 @@
 @endsection
 
 @section('main-content')
-    
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                @foreach ($comics as $comic)
+                    
+                <div class="col">
+                    <div class="card">
+                        <img src="{{$comic['thumb']}}" class="card-img-top" alt="{{$comic['title']}}">
+                        <div class="card-body">
+                          <h5 class="card-title">{{$comic['title']}}</h5>
+                          <h6 class="card-subtitle mb-2 text-body-secondary">{{$comic['series']}}</h6>
+                          <p class="card-text">{{$comic['description']}}</p>
+                          <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                      </div>
+                </div>
+
+                @endforeach
+
+            </div>
+        </div>
+    </div>
 @endsection
